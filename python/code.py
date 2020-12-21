1239d6cf71263c0e6045e49923837935b113f96e
rahulkanojiya <rahulkanojiya809@gmail.com>
2020-12-21 17:03:22 +0530

    Update code.py

--- python/code.py
+++ python/code.py
@@ -1,5 +1,3 @@
-# --------------
-# Code starts here
 
 # Create the lists 
 class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
@@ -19,56 +17,19 @@ del(new_class[5])
 
 print(new_class)
 
-# Create the Dictionary
+# Create the Dictionary marks of Geoffrey Hinton
 
 courses = {'Math' : 65 , 'English' : 70 , 'History' : 80 , 'French' : 70 ,'Science' : 60}
 total = sum(courses.values())
 percentage = 100*total/500
-print("percentage:", percentage)
+print("percentage obtained  by Geoffrey Hinton:", percentage)
+
 
 mathematics = {'Geoffrey Hinton' : 78, 'Andrew Ng':95 , 'Sebastian Raschka':65,'Yoshua Benjio' :50, 'Hilary Mason':70, 'Corinna Cortes': 66 , 'Peter Warden' : 75}
 topper = max(mathematics,key = mathematics.get)
-print(total)
+print("mathematics topper :" , topper)
 
 first_name , last_name = topper.split()
 full_name = last_name + " " + first_name
 certificate_name = full_name.upper()
-# Slice the dict and stores  the all subjects marks in variable
-
-
-# Store the all the subject in one variable `Total`
-
-# Print the total
-
-# Insert percentage formula
-
-# Print the percentage
-
-
-
-
-# Create the Dictionary
- 
-
-
-
-# Given string
-
-
-# Create variable first_name 
-
-# Create variable Last_name and store last two element in the list
-
-# Concatenate the string
-
-# print the full_name
-
-# print the name in upper case
-
-# Code ends here
-
-# print the name in upper case
-
-# Code ends here
-
-
+print(certificate_name)
